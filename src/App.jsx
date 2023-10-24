8af2c5e2c178c8e8dc5e7f4f67c6641d8c8781af
calebmcmains <calebmcmains@gmail.com>
2023-10-23 22:27:14 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {
+  DragFramer,
+  HoverFramer,
+  SpeedText,
+  SpinningShapeShiftingDiv,
+} from "./components/FramerTest";
+
+export default function App() {
+  return (
+    <>
+      <div className="flex items-center">
+        {/* <SpinningShapeShiftingDiv />
+        <HoverFramer /> */}
+        {/* <DragFramer letter={"N"} />
+        <DragFramer letter={"O"} />
+        <DragFramer letter={"R"} />
+        <DragFramer letter={"A"} /> */}
+        <SpeedText />
+      </div>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "ui-sans-serif", "system-ui"],
+      sans2: ['"Inter Tight"', "ui-sans-serif", "system-ui"],
+      serif: ['"Instrument Serif"', "ui-serif", "Georgia"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
