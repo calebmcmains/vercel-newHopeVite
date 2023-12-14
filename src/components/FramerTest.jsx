09ad11cca5f07fc660cfc8a3d1eec8782e40fb94
calebmcmains <calebmcmains@gmail.com>
2023-12-14 00:26:39 -0700

    change to calebmcmains

--- src/components/FramerTest.jsx
+++ src/components/FramerTest.jsx
@@ -134,13 +134,13 @@ export function SpeedText() {
     <section className="h-screen">
       <ParallaxText baseVelocity={1}>
         <p className="me-2 inline font-sans2 text-[1rem] italic tracking-wider">
-          CALE
+          CALEB
         </p>
         <p className="inline  text-[1.5rem]">mcmains</p>
       </ParallaxText>
       <ParallaxText baseVelocity={-1}>
         <p className="me-2 inline font-sans2 text-[1rem] italic tracking-wider">
-          CALE
+          CALEB
         </p>
         <p className="inline  text-[1.5rem]">mcmains</p>
       </ParallaxText>
